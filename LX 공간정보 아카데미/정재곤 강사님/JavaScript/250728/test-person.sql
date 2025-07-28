678af634fa7adafbbd665ebfb64a0157cb4e39c5
junha13 <tnznf1113@gmail.com>
2025-07-28 20:14:30 +0900

    개발 공부

--- LX 공간정보 아카데미/정재곤 강사님/JavaScript/250728/test-person.sql
+++ LX 공간정보 아카데미/정재곤 강사님/JavaScript/250728/test-person.sql
@@ -1,2 +1,13 @@
 SELECT id, NAME, age, mobile
 FROM test.person;
+
+Insert into test.person(name, age, mobile)
+values
+('홍길동', 22, '010-1111-1111');
+
+Update test.person 
+set mobile = '010-2222-2222'
+where id = 2;
+
+delete from test.person
+where id = 2;
