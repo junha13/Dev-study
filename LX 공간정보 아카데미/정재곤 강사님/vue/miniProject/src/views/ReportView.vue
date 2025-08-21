a4815c83a6131a6c0ac48d2de0a54021aa4eb640
junha13 <tnznf1113@gmail.com>
2025-08-21 21:49:52 +0900

    개발 공부

--- LX 공간정보 아카데미/정재곤 강사님/vue/miniProject/src/views/ReportView.vue
+++ LX 공간정보 아카데미/정재곤 강사님/vue/miniProject/src/views/ReportView.vue
@@ -2,9 +2,62 @@
 <div>
     <div class="w-100">
         <div class="p-4">
-            <img src="/src/assets/images/grass.jpg" class="w-100 rounded-3 border-2"></img>
+            <img src="/src/assets/images/grass.jpg" class="w-100 rounded-3 border border-success border-3"></img>
         </div>
     </div>
+
+    <div class="d-flex flex-row justify-content-between">
+        <div class="w-50">  <!-- 왠지는 모르겠는데 이러니까 4:4:2로 나눠가지네-->
+            <div class="d-flex flex-column justify-content-between h-250px">
+                <div class="h-50 p-1">
+                    <div class="border dorder-3 bg-primary">
+
+
+                    </div>
+                </div>
+                <div class="h-50">
+                    dd
+                </div>
+            </div>
+        </div>
+        <div class="w-50">  <!-- 왠지는 모르겠는데 이러니까 4:4:2로 나눠가지네-->
+            <div class="d-flex flex-column justify-content-between h-250px">
+                <div class="h-50">
+                    dd
+                </div>
+                <div class="h-50">
+                    dd
+                </div>
+            </div>
+        </div>
+        <div class="w-25">
+            <div class="bg-primary">
+                <div>
+                    dd
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     <div class="d-flex flex-row justify-content-between">
         <div class="m-auto">
