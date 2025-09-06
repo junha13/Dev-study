bbd347d8573e3948aa759b0cd8b5bb29a9d08770
junha13 <tnznf1113@gmail.com>
2025-09-06 23:49:44 +0900

    spring boot user 연습

---
+++ LX 공간정보 아카데미/이희만 강사님/Spring/mysbproject/src/main/java/lx/edu/mysbproject/MysbprojectApplication.java
@@ -0,0 +1,13 @@
+package lx.edu.mysbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysbprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysbprojectApplication.class, args);
+	}
+
+}
